a958a73b3b8de960ea3fce713cef5bdf16bef555
amaksyuta <andreymaksuyta@gmail.com>
2022-05-29 01:16:32 +0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import app
 
 
 def test_add():
-    assert main.add(3, 4) == 7
-    assert main.add(3.5, 4) == 7
-    assert main.add(3.9, 4) == 7
-    assert main.add(3.9, 4.1) == 8
+    assert app.add(3, 4) == 7
+    assert app.add(3.5, 4) == 7
+    assert app.add(3.9, 4) == 7
+    assert app.add(3.9, 4.1) == 8
