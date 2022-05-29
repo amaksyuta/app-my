22507bc7ad7fcfbacc9a5aa46cb8e958f7c93a20
amaksyuta <a@itech.ua>
2022-05-29 21:10:20 +0300

    Add new functions
    Signed-off-by: amaksyuta <a@itech.ua>

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ def fib(num: int) -> int:
     return num if num < 2 else fib(num-1)+fib(num-2)
 
 
+def summ(one, two):
+    return one + two
+
+
 if __name__ == "__main__":
     main( )
-    
--- test_app.py
+++ test_app.py
@@ -2,3 +2,9 @@ import app
 
 def test_fibonacci():
     assert app.fib(10) == 55 # It was 54 before
+
+
+def test_summ():
+    assert app.summ(2, 5) == 7
+    assert app.summ(3, 4) == 7
+    assert app.summ(2, 4) == 6
