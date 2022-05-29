9f899692db891c9126b48fbcca3b5378180038c5
amaksyuta <a@itech.ua>
2022-05-29 20:50:38 +0300

    Change variables names
    Signed-off-by: amaksyuta <a@itech.ua>

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ def main( ):
     print("Hello Function")
 
 
-def fib(n: int) -> int:
-    return n if n < 2 else fib(n-1)+fib(n-2)
+def fib(num: int) -> int:
+    return num if num < 2 else fib(num-1)+fib(num-2)
 
 
 if __name__ == "__main__":
