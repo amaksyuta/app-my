46937858b43d4cbb08ec1c4321c33996d7223e79
amaksyuta <a@itech.ua>
2022-05-30 21:03:46 +0300

    Add functions and class to app tool
    Signed-off-by: amaksyuta <a@itech.ua>

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 
+import sys
+import os
 import argparse
 from __version import __version__
 
@@ -33,9 +35,27 @@ def summ(one, two):
     return one + two
 
 
+class App:
+    def __init__(self, input, output):
+        self.input = input
+        self.output = output
+        print(f"Input file name: {self.input}, Output file name: {self.output}")
+
+
+    def check_extention(self):
+        """ Check file extensions"""
+        pass
+
+    def check_file_exist(self):
+        """ Check if file exist """
+        pass
+    
+    
 def main():
     args = create_parser()
     print(f"Current parameters: {args}")
+    result = App(input=args.input, output=args.output)
+    sys.stderr.close
 
 
 if __name__ == "__main__":
