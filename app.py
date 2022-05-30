6fe89446071de0ccb2dd85b2ec64b053f94fa3a9
amaksyuta <a@itech.ua>
2022-05-30 18:20:42 +0300

    Remove whitespaces with VSCode
    Signed-off-by: amaksyuta <a@itech.ua>

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def create_parser():
     parser = argparse.ArgumentParser(
         description="Command line tool for serialise/deserialise data",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,)
-    parser.add_argument ("-i","--input", 
+    parser.add_argument ("-i","--input",
         help="Input file with personal data(name, address, phone)",
-        default="test.txt", 
+        default="test.txt",
         required=True)
-    parser.add_argument ("-o", "--output", 
-        help="Output file in supported format csv/json only)", 
+    parser.add_argument ("-o", "--output",
+        help="Output file in supported format csv/json only)",
         default="test.json",
         required=True)
-    parser.add_argument ("-v", "--version", 
-        action="version", 
+    parser.add_argument ("-v", "--version",
+        action="version",
         version=__version__)
     args = parser.parse_args()
     return args
