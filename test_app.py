c99918efe33f8cc679aa93dc958e21d4b2f00c2b
amaksyuta <a@itech.ua>
2022-06-04 04:40:22 +0300

    Fix Unit test
    Signed-off-by: amaksyuta <a@itech.ua>

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
 import app
 
 def test_get_file_extension():
-    assert app.get_file_extension("test.txt") == txt # It was 54 before
-    assert app.get_file_extension("test.json") == json
-    assert app.get_file_extension("test.csv") == csv
+    assert app.get_file_extension("test.txt") == "txt" 
+    assert app.get_file_extension("test.json") == "json"
+    assert app.get_file_extension("test.csv") == "csv"
 
 
 def test_get_file_name():
-    assert app.get_file_name("test.txt") == test
-    assert app.get_file_name("new_file.json") == new_file
-    assert app.get_file_name("new.ttf") == new
+    assert app.get_file_name("test.txt") == "test"
+    assert app.get_file_name("new_file.json") == "new_file"
+    assert app.get_file_name("new.ttf") == "new"
     
